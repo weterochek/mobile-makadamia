1e556454734ca915f9d8ecce85cbd6ff51827683
weterochek <132129419+weterochek@users.noreply.github.com>
2024-12-15 02:19:18 +0900

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:makadamia/counter_page.dart';
+import 'package:makadamia/counter_page.dart'; // Убедитесь, что файл существует и путь корректен.
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+    // Загружаем CounterPage
     await tester.pumpWidget(MaterialApp(home: CounterPage()));
 
-    // Проверяем начальное состояние
+    // Проверяем, что начальное значение равно "0"
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Нажимаем кнопку "+"
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pumpAndSettle();
+    await tester.pump();
 
-    // Проверяем состояние после нажатия
+    // Проверяем, что значение увеличилось до "1"
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
