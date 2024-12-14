e9e6489b4672db8300830fedf3d65a1df64157cb
weterochek <132129419+weterochek@users.noreply.github.com>
2024-12-14 22:39:22 +0900

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,22 +1,27 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
 import 'package:makadamia/main.dart';
 
 void main() {
+  setUpAll(() {
+    // Заглушка для платформы WebView. Убираем вызов WebView.platform
+    debugPrint("WebView platform setup skipped for test environment");
+  });
+
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
+    // Запускаем приложение
     await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
+    // Проверяем начальное состояние
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
+    // Нажимаем кнопку "+"
     await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    await tester.pumpAndSettle(); // Ждем завершения всех обновлений
 
-    // Verify that our counter has incremented.
+    // Проверяем состояние после нажатия
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
   });
